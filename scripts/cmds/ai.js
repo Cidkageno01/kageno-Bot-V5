25b7009846a06c869559bc15525d62673a7b8d13
⫷㊚ç𝖎𝖉×͜×𝖐𝖆𝖌𝖊𝖓𝖔㊚⫸ <sorokomarco@gmail.com>
2024-08-07 17:02:26 +0000

    Update ai.js

--- scripts/cmds/ai.js
+++ scripts/cmds/ai.js
@@ -1,24 +1,66 @@
-#cmd install ai.js module.exports = {
-	config: {
-		name: "ai",
-		aliases: ["ai"],
-		version: "1.0",
-		author: "ʬɸʬ Sønïč Shïsûį ʬɸʬ", // do not change this credits
-		countDown: 5,
-		role: 0,
-		shortDescription: "send you pic of fleetway super Sonic ",
-		longDescription: "send you pic of fleetway super Sonic",
-		category: "no prefix",
-		guide: "{pn}"
-	},
-
-	onStart: async function ({ message }) {
-	 var link = [ 
-"https://i.ibb.co/6XMQ7rs/image.jpg",
-  ]
-let img = link[Math.floor(Math.random()*link.length)]
-message.send({
-  body: "𝐨𝐧 𝐝𝐢𝐭 𝐩𝐚𝐬 𝐚𝐢 𝐚𝐛𝐫𝐮𝐭𝐢𝐬 ",attachment: await global.utils.getStreamFromURL(img)
-})
+const { getPrefix, getStreamFromURL, uploadImgbb } = global.utils;
+async function ai99({ message: m, event: e, args: a, usersData: u }) {
+	var p = [`${await getPrefix(e.threadID)}${this.config.name}`,
+`${this.config.name}`
+/*"ai"
+*you can add more prefix here
+*/
+]; 
+ if (p.some(b => a[0].toLowerCase().startsWith(b))) {
+try {      
+let prompt = "";
+if (e.type === "message_reply" && e.messageReply.attachments && e.messageReply.attachments[0]?.type === "photo") {
+ const b = await uploadImgbb(e.messageReply.attachments[0].url);
+prompt = a.slice(1).join(" ") + ' ' + b.image.url;
+} else {
+ prompt = a.slice(1).join(" ");
 }
+ var __ = [{ id: e.senderID, tag: await u.getName(e.senderID) }];
+ const r = await require("axios").post(`https://test-ai-ihc6.onrender.com/api`, {
+	prompt: prompt,
+ apikey: "GayKey-oWHmMb1t8ASljhpgSSUI",
+	name: __[0]['tag'],
+ id: __[0]['id'],
+ });
+var _ = r.data.result.replace(/{name}/g, __[0]['tag']).replace(/{pn}/g, p[0]);
+ if (r.data.av) {
+ if (Array.isArray(r.data.av)) {
+ const avs = r.data.av.map(url => getStreamFromURL(url));
+ const avss = await Promise.all(avs);
+	m.reply({
+ body: _,
+ mentions: __,
+ attachment: avss
+ });
+ } else {
+ m.reply({
+ body: _,
+ mentions: __,
+attachment: await getStreamFromURL(r.data.av)
+	});
+	}
+	} else {
+m.reply({
+body: _,
+mentions: __
+	});
+	}
+	} catch (error) {
+ m.reply("Error " + error);
+ }
+ }
 }
+module.exports = {
+config: {
+ name: "ai99",
+aliases: [ai],
+version: 1.6,
+author: "Jun",
+role: 0,
+ shortDescription: "An AI that can do various tasks",
+ guide: "{pn} <query>",
+ category: "AI"
+ },
+ onStart: function() {},
+ onChat: ai
+};
